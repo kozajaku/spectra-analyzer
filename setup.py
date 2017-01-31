f7e1c645e2a172c2c1de913db2e06251e3b26194
Jakub Koza <kozajaku@fit.cvut.cz>
2017-01-31 22:31:41 +0100

    Created modular structure
    Added files necessary for Python module installation and execution

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='spectra_analyzer',
+    version='0.1',
+    description='WebSocket based web application for downloading and analyzing astronomical spectra.',
+    long_description=long_description,
+    author='Jakub Koza',
+    author_email='kozajaku@fit.cvut.cz',
+    keywords='astronomy,spectra,spectrum',
+    license='MIT',
+    url='https://github.com/kozajaku/spectra-analyzer',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spectra_analyzer = spectra_analyzer.server:main',
+        ],
+    },
+    install_requires=['spectra_downloader', 'flask', 'eventlet', 'flask-socketio',
+                      'astropy', 'numpy', 'matplotlib'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'flexmock', 'betamax'],
+)
