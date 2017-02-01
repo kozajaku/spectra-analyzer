9409c7343dc233401880a98c7618c83e810abd7a
Jakub Koza <kozajaku@fit.cvut.cz>
2017-02-01 01:35:23 +0100

    Forced numpy version
    Continuous wavelet transformation method (method autoscale) implemented
    in mlpy library does not work with newer numpy version.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         ],
     },
     install_requires=['spectra_downloader', 'flask', 'eventlet', 'flask-socketio',
-                      'astropy', 'numpy', 'matplotlib'],
+                      'astropy', 'numpy==1.11.2', 'matplotlib'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'flexmock', 'betamax'],
 )
